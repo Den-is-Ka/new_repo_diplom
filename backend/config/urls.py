96bbc324c4b1fdfb89dc88d7f4558e919d564053
user <d_k81@mail.ru>
2026-02-05 21:02:38 +0500

    стартовый коммит начало Диплома

---
+++ backend/config/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+
+    # OpenAPI schema
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+
+    # Swagger UI
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+]
